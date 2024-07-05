b898fe7ab27f7e0e6c78f843adc02ec5bfd4f7e7
seo hyun seok <tjgustjr33@naver.com>
2024-07-05 22:56:22 +0900

    feat:팔로워,팔로잉 수, 경고문구 추가, refactor:비번 확인필드 제거
    유저 필드에 팔로워 수, 팔로잉 수 필드 추가, blank가 Fasle인 필드를 빈 문자열로 제출 시 에러 메세지 기능 추가
    
    비밀번호 확인 필드 제거, 비밀번호 확인 관련 함수 전부 제거

--- users/serializers.py
+++ users/serializers.py
@@ -4,25 +4,26 @@ from users.models import User
 
 class UserSerializer(serializers.ModelSerializer):
     posts_num = serializers.SerializerMethodField()
-    password_confirm = serializers.CharField(write_only=True, required=True)
+    followers_count = serializers.SerializerMethodField()
+    following_count = serializers.SerializerMethodField()
     
     class Meta:
         model = User
-        fields = ['id','username', 'nickname', 'email', 'gender', 'age', 'profile_image', 'password', 'password_confirm', 'posts_num']
+        fields = ['id','username', 'nickname', 'email', 'gender', 'age', 'profile_image', 'password', 'followers_count', 'following_count', 'posts_num']
         extra_kwargs = {
-            'username': {'write_only': True},
-            'password': {'write_only': True}
+            'username': {'write_only': True, 'error_messages': {'blank': '아이디를 입력해야 합니다.'}},
+            'password': {'write_only': True, 'error_messages': {'blank': '비밀번호를 입력해야 합니다.'}},
+            'nickname': {'error_messages': {'blank': '닉네임을 입력해야 합니다.'}},
+            'email': {'error_messages': {'blank': '이메일을 입력해야 합니다.'}},
+            'age': {'error_messages': {'invalid': '나이를 선택해야 합니다.'}},
         }
 
     # 유저의 포스트 개수
     def get_posts_num(self, obj):
         return Post.objects.filter(user=obj).count()
-    
-    def validate(self, data):
-        if data['password'] != data['password_confirm']:
-            raise serializers.ValidationError("비밀번호가 일치하지 않습니다")
-        return data
-    def create(self, validated_data):
-        validated_data.pop('password_confirm')
-        user = User.objects.create_user(**validated_data)
-        return user
+    # 유저의 팔로워 수
+    def get_followers_count(self, obj):
+        return obj.followers.count()
+    # 유저의 팔로잉 수
+    def get_following_count(self, obj):
+        return obj.followings.count()
