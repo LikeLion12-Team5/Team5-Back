234f2d78c4135daf742bc162dc0a1a8d9b282e34
seo hyun seok <tjgustjr33@naver.com>
2024-07-12 13:24:26 +0900

    feat: config

--- config/settings.py
+++ config/settings.py
@@ -91,10 +91,8 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
-    "http://3.39.171.235:8000",
     "http://localhost:5500",
     "http://127.0.0.1:5500",
-    "http://3.39.171.235:5500",
 ]
 
 from datetime import timedelta
